81730f28ecbb38200f1df393132ebd072ad938a0
Katsushi OUGI <ougi@to-r.net>
2022-01-28 23:13:23 +0900

    webpack追加

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const PostCSSPresetEnv = require('postcss-preset-env');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+
+  entry: "./src/assets/webpack/main.js",
+
+  output: {
+    path: `${__dirname}/_site/assets/webpack`,
+    filename: 'main.js'
+  },
+
+  plugins: [new MiniCssExtractPlugin()],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s?css/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  PostCSSPresetEnv()
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+    ],
+  },
+
+  externals: {
+    // 外部のjQueryを利用する
+    // 利用したい場合は import $ from 'jquery'; とする
+    jquery: 'jQuery'
+  },
+};
